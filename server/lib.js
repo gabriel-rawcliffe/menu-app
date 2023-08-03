07813bd3ac9e55b06d894349baf34ed5a3eda7d0
Stanley Mair <stanleygmair@gmail.com>
2023-08-04 11:38:36 +1200

    Moved js from lib to new file

--- server/lib.js
+++ server/lib.js
@@ -22,31 +22,3 @@ export async function writeRecipes(data) {
     console.error(err.message)
   }
 }
-
-// Form add ingredients button
-const addIngredientButton = document.getElementById('add-ingredient')
-const ingredientInputContainer = document.getElementById('ingredient-input')
-const amountInputContainer = document.getElementById('amount-input')
-
-function addIngredientInput() {
-  const newInput = document.createElement('input')
-  newInput.setAttribute('type', 'text')
-  newInput.setAttribute('name', 'ingredient[]')
-  newInput.setAttribute('value', '')
-  ingredientInputContainer.appendChild(newInput)
-}
-
-function addAmountInput() {
-  const newInput = document.createElement('input')
-  newInput.setAttribute('type', 'text')
-  newInput.setAttribute('name', 'amount[]')
-  newInput.setAttribute('value', '')
-  amountInputContainer.appendChild(newInput)
-}
-
-function combineTwo() {
-  addIngredientInput()
-  addAmountInput()
-}
-
-addIngredientButton.addEventListener('click', combineTwo)
